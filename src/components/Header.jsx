713aebdf1bd5c1ff29e89d2363f13e469a6a18eb
dilpreet-r <dilpreetr18@example.com>
2020-06-27 01:26:55 +0530

    commiting changes

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NotificationsIcon from '@material-ui/icons/Notifications';
+function Header() {
+  return (
+    <header>
+      <h1>
+        <NotificationsIcon />
+        QuickNote</h1>
+    </header>
+  );
+}
+
+export default Header;
